620d68c90c7c15d6692a3abea6d4e8793e94a373
Tim Douglas <neurotech@gmail.com>
2014-03-25 09:06:56 +1100

    Proof of concept

---
+++ proverbs.js
@@ -0,0 +1,60 @@
+// Modules
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+
+var app = express();
+
+// Express Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+var filepath = path.join(__dirname, '/proverbs.json');
+var proverbs = JSON.parse(fs.readFileSync(filepath));
+
+function randomProverb() {
+  return proverbs.sort(function() {return 0.5 - Math.random()})[1];
+};
+
+function health() {
+  return {
+    pid: process.pid,
+    memory: process.memoryUsage(),
+    uptime: process.uptime()
+  };
+}
+
+app.get('/', function(req, res) {
+  res.json(200, randomProverb());
+});
+
+app.get('/json', function(req, res) {
+  res.json(200, randomProverb());
+});
+
+app.get('/json/all', function(req, res) {
+  res.json(200, proverbs);
+});
+
+app.get('/health', function(req, res){
+  res.json(200, health());
+});
+
+app.listen(app.get('port'), function (err, status) {
+  if (err) {
+    console.log(err);
+  }
+});
