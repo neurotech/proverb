bcc47fd42c073d6b3efb28100f32079644c476d6
Tim Douglas <neurotech@gmail.com>
2014-03-25 09:37:24 +1100

    Added TODO comments, refactored dir structure, added random proverb as vars

--- proverbs.js
+++ proverbs.js
@@ -1,3 +1,14 @@
+// =========
+// TODO
+// =========
+
+// - Create array of past-tense verbs, adjectives, nouns to be randomly grouped for attribution line.
+//   - i.e. 'This proverb {verb} to your {adjective} {noun} by {episode} - '{name}'.'
+// - Improve health function
+// - Jade template polish
+// - Stylus files polish
+
+
 // Modules
 var express = require('express');
 var fs = require('fs');
@@ -7,15 +18,14 @@ var app = express();
 
 // Express Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(require('stylus').middleware(path.join(__dirname, '/src/stylesheets')));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Development only
@@ -35,10 +45,14 @@ function health() {
     memory: process.memoryUsage(),
     uptime: process.uptime()
   };
-}
+};
 
 app.get('/', function(req, res) {
-  res.json(200, randomProverb());
+  res.render('index', {
+    episode: randomProverb().episode,
+    name: randomProverb().title,
+    proverb: randomProverb().proverb
+  });
 });
 
 app.get('/json', function(req, res) {
